988e7ddc1695b03d4d80ba7e0a292e469eb5e7c9
raghadbar <31386396+raghadbar@users.noreply.github.com>
2017-08-27 06:23:27 -0700

    Update Login2.php

--- Login2.php
+++ Login2.php
@@ -3,23 +3,23 @@
 
     $con = mysqli_connect("my_host", "my_user", "my_password", "my_database");
     
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ?");
-    mysqli_stmt_bind_param($statement, "s", $username);
+    $statement = mysqli_prepare($con, "SELECT * FROM admin WHERE username = ?");
+    mysqli_stmt_bind_param($statement, "s", $first_name);
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $colUserID, $colName, $colUsername, $colAge, $colPassword);
+    mysqli_stmt_bind_result($statement, $colfirst_name, $collast_name, $colemail, $colphone, $colpass);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
-        if (password_verify($password, $colPassword)) {
+        if (password_verify($pass, $colpass)) {
             $response["success"] = true;  
-            $response["name"] = $colName;
-            $response["age"] = $colAge;
+            $response["first_name"] = $colfirst_name;
+            $response["last_name"] = $collast_name;
         }
     }
 
